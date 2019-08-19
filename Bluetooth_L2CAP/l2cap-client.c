f0a71e9623c0f54bd1026e3ab685c10cac3f855e
Gang-Ho, LEE <happy96115@gmail.com>
2019-08-19 22:16:26 +0900

    Update l2cap-client.c

--- Bluetooth_L2CAP/l2cap-client.c
+++ Bluetooth_L2CAP/l2cap-client.c
@@ -1,32 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/l2cap.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include "PiCamera.h"
+#include "ServoCtl.h"
 
 #define BUF_SIZE 672
+#define PAN 12
+#define TILT 13
 
 int main(int argc, char **argv)
 {
 	struct sockaddr_l2 addr = { 0 };
 	char buf[BUF_SIZE];
+	char file_name[BUF_SIZE];
 	int sourse_fd;
 	int file_name_len, read_len;
 	int s, status;
-	//    char *message = "hello!";
+	char *ptr;
 	char dest[18] = "B8:27:EB:75:79:7A";
-
-	/*
-		if(argc < 2)
-		{
-			fprintf(stderr, "usage: %s <bt_addr>\n", argv[0]);
-			exit(2);
-		}
-
-		strncpy(dest, argv[1], 18);
-	*/
 
 	// allocate a socket
 	s = socket(AF_BLUETOOTH, SOCK_SEQPACKET, BTPROTO_L2CAP);
@@ -39,35 +35,42 @@ int main(int argc, char **argv)
 	// connect to server
 	status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
 
-	memset(buf, 0x00, BUF_SIZE);
-	printf("File Name : Pub.jpg");
-	strcpy(buf, "Pub.jpg");
-	file_name_len = strlen(buf);
 
-	status = write(s, buf, file_name_len);
-	sourse_fd = open(buf, O_RDONLY);
-	if (!sourse_fd) {
-		perror("Error : ");
-		return 1;
-	}
-
-	while (1) {
+	while(1) {
 		memset(buf, 0x00, BUF_SIZE);
-		read_len = read(sourse_fd, buf, BUF_SIZE);
-		status = write(s, buf, BUF_SIZE);
-		if (status < 0) perror("uh oh");
-		if (read_len == 0)	break;
-	}
-	// send a message
-	/*
-	if (status == 0) {
-		status = write(s, "hello!", 6);
-	}
+		memset(file_name, 0x00, BUF_SIZE);
+	
+		// read Angle
+		printf("Reanding....\n");
+		read_len = read(s, buf, BUF_SIZE);
+		//printf("%s\n", buf);
 
-	if (status < 0) perror("uh oh");
-	*/
+		// send Angle
+		setAngle(buf);
+
+		// take Picture
+		takePic(file_name);
+
+		memset(buf, 0x00, BUF_SIZE);
+		printf("File Name : %s\n", file_name);
+		file_name_len = strlen(file_name);
+
+		status = write(s, file_name, file_name_len);
+
+		sourse_fd = open(file_name, O_RDONLY);
+		if (!sourse_fd) {
+			perror("Error : ");
+			return 1;
+		}
+
+		while (1) {
+			memset(buf, 0x00, BUF_SIZE);
+			read_len = read(sourse_fd, buf, BUF_SIZE);
+			status = write(s, buf, BUF_SIZE);
+			if (status < 0) perror("uh oh");
+			if (read_len == 0)	break;
+		}
+	}
 
 	close(s);
 }
-
-
