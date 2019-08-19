6cbc1d4320bd60139d5ca5d7eb8e91062eeab82a
Gang-Ho, LEE <happy96115@gmail.com>
2019-08-19 22:16:04 +0900

    Update ServoCtl.h

--- Sensor/ServoCtl.h
+++ Sensor/ServoCtl.h
@@ -1,28 +1,71 @@
-#include <stdio.h>
-#include <wiringPi.h>
-#include <softPwm.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
-int initServo(int PIN) {
+/*
+void servoControl(char * angle) {
+        char buf[1024];
 
-        if (wiringPiSetupPhys() == -1)  return -1;
+	memset(buf, 0, 1024);
+	// make buf to "echo 1=50% > /dev/servoblaster"
+	strcat(buf, "echo ");
+	strcat(buf, angle);
+	strcat(buf, "% > /dev/servoblaster");
 
-        pinMode(PIN, PWM_OUTPUT);
+	// on servoblaster
+	system("sudo ./servod");
+	system(buf);
 
-        pwmSetMode(PWM_MODE_MS);
+	// wait servo turn exactly
+	sleep(1);
 
-        pwmSetClock(384);
-        pwmSetRange(1000);
-
-        return 0;
+	// off servoblaster (save power)
+	system("sudo killall servod");
 }
+*/
 
-int changePWM(int PIN, float PWM) {
-        if (PWM >= 30 && PWM <= 120) {
-                pwmWrite(PIN, PWM);
-                sleep(1);
-                return 0;
-        }
-        return -1;
+void setAngle(char * angles) {
+        // angles example ("1=50 2=30")
+	char angle_PAN[10];
+	char angle_TILT[10];
+	char buf[672];
+
+	memset(angle_PAN, 0x00, 10);
+	memset(angle_TILT, 0x00, 10);
+
+        // ptr -> "1=50"
+        char *ptr = strtok(angles, " ");
+        printf("%s\n", ptr);
+	strcat(angle_PAN, ptr);
+
+        ///ptr -> "2=30"
+        ptr = strtok(NULL, " ");
+        printf("%s\n", ptr);
+	strcat(angle_TILT, ptr);
+
+
+	// on servoblaster
+	system("sudo ./servod");
+
+	// set angle_PAN
+	memset(buf, 0x00, 672);
+	strcat(buf, "echo ");
+        strcat(buf, angle_PAN);
+        strcat(buf, "% > /dev/servoblaster");
+	system(buf);
+	printf("angle_PAN %s\n", buf);
+
+	// set angle_TILT
+	memset(buf, 0x00, 672);
+        strcat(buf, "echo ");
+        strcat(buf, angle_TILT);
+        strcat(buf, "% > /dev/servoblaster");
+	system(buf);
+	printf("angle_TILT %s\n", buf);
+
+	// wait servo turn exactly
+	sleep(1);
+
+	// off servoblaster (save power)
+        system("sudo killall servod");
+
 }
